0efa6e05fcd83ab6957e74e9b002520b461d7978
Dorigami <72416919+Dorigami@users.noreply.github.com>
2024-05-19 02:22:50 -0500

    s

--- wfc_sketch.js
+++ wfc_sketch.js
@@ -43,8 +43,6 @@ window.addEventListener('resize', () => {
   draw(canvas)
 })
 
-
-
 function preloadImages() {
   // const path = './assets/tiles/circuit-coding-train';
   const path = './assets/tiles/new-circuit';
@@ -57,22 +55,6 @@ function preloadImages() {
 }
 
 function preloadTiles(){
-  /*
-  // Loaded and created the tiles
-  tiles[0] = new Tile(tileImages[0], ['AAA', 'AAA', 'AAA', 'AAA']);
-  tiles[1] = new Tile(tileImages[1], ['BBB', 'BBB', 'BBB', 'BBB']);
-  tiles[2] = new Tile(tileImages[2], ['BBB', 'BCB', 'BBB', 'BBB']);
-  tiles[3] = new Tile(tileImages[3], ['BBB', 'BDB', 'BBB', 'BDB']);
-  tiles[4] = new Tile(tileImages[4], ['ABB', 'BCB', 'BBA', 'AAA']);
-  tiles[5] = new Tile(tileImages[5], ['ABB', 'BBB', 'BBB', 'BBA']);
-  tiles[6] = new Tile(tileImages[6], ['BBB', 'BCB', 'BBB', 'BCB']);
-  tiles[7] = new Tile(tileImages[7], ['BDB', 'BCB', 'BDB', 'BCB']);
-  tiles[8] = new Tile(tileImages[8], ['BDB', 'BBB', 'BCB', 'BBB']);
-  tiles[9] = new Tile(tileImages[9], ['BCB', 'BCB', 'BBB', 'BCB']);
-  tiles[10] = new Tile(tileImages[10], ['BCB', 'BCB', 'BCB', 'BCB']);
-  tiles[11] = new Tile(tileImages[11], ['BCB', 'BCB', 'BBB', 'BBB']);
-  tiles[12] = new Tile(tileImages[12], ['BBB', 'BCB', 'BBB', 'BCB']);
-  */
   // Loaded and created the tiles
   tiles[0]  = new Tile(tileImages[0],  ['___', '___', '___', '___']);
   tiles[1]  = new Tile(tileImages[1],  ['___', '___', '_B_', '___']);
